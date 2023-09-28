59bc45512f33e9b9372775326fc0b04fa4be89ef
pupil <pupil@ivkhk.ee>
2023-09-28 11:17:26 +0300

    fixed bugs

--- src/jptv22game/App.java
+++ src/jptv22game/App.java
@@ -12,35 +12,57 @@ import java.util.Scanner;
  *
  * @author pupil
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
 
     public void run() {
         System.out.println("--- Game ---");
         Random random = new Random();
         boolean repeat = true;
-        int attemps = 3;
-        do{
-        int myNumber = random.nextInt (9-0+1)-0;
-        System.out.println("Задумано число от 0 до 9, угадай какое: ");
-        Scanner scanner = new Scanner(System.in);
-        int userNumber = scanner.nextInt();scanner.nextLine();
-        if(myNumber == userNumber) {
-            System.out.println("Ты выйграл! Ура!");
-        }else{
-            System.out.println("Ты проиграл :(");
-            attemps = attemps - 1;
-            System.out.println("Попыток осталось: "+attemps);
-            if (attemps <= 0){
-            System.out.println("Ваши попытки закончились!");
-            System.out.println("Нажмите буквy q для выхода, любую другую клавишу чтобы продолжить");
-        String q = scanner.nextLine();
-        if(q.equals("q")){
-            repeat = false;
-        }
-        System.out.println("--- End Game ---");
+        int attempts = 3;
+
+        do {
+            int myNumber = random.nextInt(10); // Генерируем число от 0 до 9
+            System.out.println("Задумано число от 0 до 9, угадай какое: ");
             
-        }
-        }
-    }while(repeat);
-}
+            Scanner scanner = new Scanner(System.in);
+            int userNumber;
+
+            while (true) {
+                try {
+                    userNumber = Integer.parseInt(scanner.nextLine());
+                    if (userNumber >= 0 && userNumber <= 9) {
+                        break; // Введенное значение верное, выходим из цикла
+                    } else {
+                        System.out.println("Пожалуйста, введите число от 0 до 9.");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Неправильный ввод. Пожалуйста, введите целое число.");
+                }
+            }
+
+            if (myNumber == userNumber) {
+                System.out.println("Ты выиграл! Ура!");
+                repeat = false; // Завершаем игру, так как пользователь угадал число
+            } else {
+                System.out.println("Ты проиграл :(");
+                attempts--;
+                System.out.println("Попыток осталось: " + attempts);
+
+                if (attempts <= 0) {
+                    System.out.println("Ваши попытки закончились!");
+                    System.out.println("Нажмите букву 'q' для выхода, любую другую клавишу чтобы продолжить");
+                    String q = scanner.nextLine();
+
+                    if (q.equals("q")) {
+                        repeat = false;
+                    }
+                }
+            }
+        } while (repeat);
+
+        System.out.println("--- End Game ---");
+    }
 }
